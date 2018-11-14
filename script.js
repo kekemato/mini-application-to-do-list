5f32e4bb6e96426a36306e795b28541e747919e4
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-14 20:40:37 +0100

    made buttons showing all, completed and uncompleted tasks

--- script.js
+++ script.js
@@ -2,14 +2,54 @@ class ToDo {
     constructor(location) {
         this.tasks = JSON.parse(localStorage.getItem('toDoList')) || []
         this.location = document.querySelector(location) || document.body
+        this.init()
+    }
+
+    init() {
         this.render()
     }
 
-    addTask(text) {
-        this.tasks.push(new Task(text))
+    makeUI() {
+        const addTaskInput = document.createElement('input')
+        const addTaskButton = document.createElement('button')
+        const searchTaskInput = document.createElement('input')
+        const searchTaskButton = document.createElement('button')
+        const filterButtonsContainer = document.createElement('p')
+        const showAllButton = document.createElement('button')
+        const showCompletedButton = document.createElement('button')
+        const showUncompletedButton = document.createElement('button')
 
-        this.updateListInLocalStorage()
-        this.render()
+        addTaskButton.innerText = 'Dodaj zadanie'
+        addTaskButton.style.marginLeft = '5px'
+
+        searchTaskInput.style.marginLeft = '20px'
+
+        searchTaskButton.innerText = 'Wyszukaj'
+        searchTaskButton.style.marginLeft = '5px'
+
+        addTaskButton.addEventListener('click', () => this.addTask(addTaskInput.value))
+        searchTaskButton.addEventListener('click', () => this.findTask(searchTaskInput.value))
+
+        showAllButton.innerText = "Pokaż wszystkie"
+        showUncompletedButton.innerText = "Pokaż nieukończone"
+        showCompletedButton.innerText = "Pokaż ukończone"
+
+        showUncompletedButton.style.marginLeft = "15px"
+        showCompletedButton.style.marginLeft = "15px"
+
+        showAllButton.addEventListener('click', () => this.render())
+        showCompletedButton.addEventListener('click', () => this.findCompletedTasks())
+        showUncompletedButton.addEventListener('click', () => this.findUncompletedTasks())
+
+        document.body.appendChild(addTaskInput)
+        document.body.appendChild(addTaskButton)
+        document.body.appendChild(searchTaskInput)
+        document.body.appendChild(searchTaskButton)
+        filterButtonsContainer.appendChild(showAllButton)
+        filterButtonsContainer.appendChild(showCompletedButton)
+        filterButtonsContainer.appendChild(showUncompletedButton)
+        document.body.appendChild(filterButtonsContainer)
+
     }
 
     render(arr) {
@@ -28,11 +68,11 @@ class ToDo {
             button.style.marginLeft = '10px'
 
             li.addEventListener('click', function () {
-                if (task.isComplited === true) {
-                    task.toggleTask()
+                if (task.isCompleted === true) {
+                    task.isCompleted = false
                     this.style.textDecoration = "none"
                 } else {
-                    task.toggleTask()
+                    task.isCompleted = true
                     this.style.textDecoration = "line-through"
                 }
             })
@@ -46,27 +86,11 @@ class ToDo {
         this.location.appendChild(ul)
     }
 
-    makeUI() {
-        const addTaskInput = document.createElement('input')
-        const addTaskButton = document.createElement('button')
-        const searchTaskInput = document.createElement('input')
-        const searchTaskButton = document.createElement('button')
+    addTask(text) {
+        this.tasks.push(new Task(text))
 
-        addTaskButton.innerText = 'Dodaj zadanie'
-        addTaskButton.style.marginLeft = '5px'
-
-        searchTaskInput.style.marginLeft = '20px'
-
-        searchTaskButton.innerText = 'Wyszukaj'
-        searchTaskButton.style.marginLeft = '5px'
-
-        addTaskButton.addEventListener('click', () => this.addTask(addTaskInput.value))
-        searchTaskButton.addEventListener('click', () => this.findTask(searchTaskInput.value))
-
-        document.body.appendChild(addTaskInput)
-        document.body.appendChild(addTaskButton)
-        document.body.appendChild(searchTaskInput)
-        document.body.appendChild(searchTaskButton)
+        this.updateListInLocalStorage()
+        this.render()
     }
 
     deleteClickHandler(e, index) {
@@ -78,8 +102,18 @@ class ToDo {
     }
 
     findTask(input) {
-        this.tasksFiltered = this.tasks.filter(task => task.text.replace(/\s/g, '').toLowerCase().includes(input.replace(/\s/g, '').toLowerCase()))
-        this.render(this.tasksFiltered)
+        const tasksFiltered = this.tasks.filter(task => task.text.replace(/\s/g, '').toLowerCase().includes(input.replace(/\s/g, '').toLowerCase()))
+        this.render(tasksFiltered)
+    }
+
+    findCompletedTasks() {
+        const completedTasks = this.tasks.filter(task => task.isCompleted === true)
+        this.render(completedTasks)
+    }
+
+    findUncompletedTasks() {
+        const uncompletedTasks = this.tasks.filter(task => task.isCompleted === false)
+        this.render(uncompletedTasks)
     }
 
     updateListInLocalStorage() {
@@ -91,13 +125,8 @@ class ToDo {
 class Task {
     constructor(text) {
         this.text = text
-        this.isComplited = false
+        this.isCompleted = false
     }
-
-    toggleTask() {
-        this.isComplited = !this.isComplited
-    }
-
 }
 
 const ToDo1 = new ToDo()
